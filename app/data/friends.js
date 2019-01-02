9c33d5342ed3bd88bcb1af2e8c6e09c887a64071
Osman Hassan <ama@Amas-MacBook-Pro.local>
2019-01-01 22:39:59 -0600

    Fixes

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Michael",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/a/ae/Michael_Jordan_in_2014.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Shania",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/e/ee/ShaniaTwainJunoAwardsMar2011.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Maya",
+		photo: "https://heightline.com/wp-content/uploads/maya-3.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Oscar",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Oscar_Robertson_1960.jpeg/440px-Oscar_Robertson_1960.jpeg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Iman",
+		photo: "http://www.bradbranson.net/wp-content/gallery/large-7/Iman-001-Edit.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
